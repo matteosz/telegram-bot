66980914e20b4545865e7926f40f4d8189ad9515
matteosz <matteosuez@libero.it>
2022-07-25 20:06:29 +0200

    Heroku directory fix

--- driver.py
+++ driver.py
@@ -23,8 +23,8 @@ def create_driver():
 
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     options.add_argument('--headless')
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--no-sandbox")
+    #options.add_argument("--disable-dev-shm-usage")
+    #options.add_argument("--no-sandbox")
 
     return webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),options=options)
 
